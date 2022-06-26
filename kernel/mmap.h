bbf2050926bbccb096a03401f9ef1b060be5680d
Eloy AM <eloy.abellanm@um.es>
2022-06-26 23:33:37 +0200

    m(un)map int as input. Mmaptest fd1 bad use fixed

--- kernel/mmap.h
+++ kernel/mmap.h
@@ -1,5 +1,6 @@
-#define PROT_READ   1
-#define PROT_WRITE  2
-#define MAP_SHARED  0
-#define MAP_PRIVATE 1
+#define PROT_READ   0x1
+#define PROT_WRITE  0x2
+#define MAP_SHARED  0x1
+#define MAP_PRIVATE 0x2
+// Equivalent to ((uint64) -1)
 #define MAP_ERROR   0xffffffffffffffff
